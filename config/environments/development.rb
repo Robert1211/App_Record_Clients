66d8d5962c637a0ab61148e4588811f40fc1be89
Robert <maculewicz.au@gmail.com>
2023-09-04 13:38:33 +1000

    update@mobile_footer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,10 +13,10 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  config.file_watcher = ActiveSupport::FileUpdateChecker
   # Enable server timing
   config.server_timing = true
-
+  config.serve_static_assets = false
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.serve_static_assets = false
 end
